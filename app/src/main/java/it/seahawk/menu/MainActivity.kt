186bb64c80b32f5c6fbe2e714576eff59c2b11e7
itchrand <ac_is_nowhere@yahoo.com>
2017-11-08 10:58:06 +0100

    Initial commit

---
+++ app/src/main/java/it/seahawk/menu/MainActivity.kt
@@ -0,0 +1,22 @@
+package it.seahawk.menu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun printToLogs(view: View) {
+        // Find first menu item TextView and print the text to the logs
+
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
+}
